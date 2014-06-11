2a7ad4381a520d8eeeb493241100684d97f4f6b3
Harry Ng <harryworld@gmail.com>
2014-06-11 20:37:28 +0800

    Initial Commit

---
+++ KODW-Tab/FirstViewController.m
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.m
+//  KODW-Tab
+//
+//  Created by Harry Ng on 11/6/14.
+//  Copyright (c) 2014 Request. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+            
+
+@end
+
+@implementation FirstViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ KODW-Tab/SecondViewController.m
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.m
+//  KODW-Tab
+//
+//  Created by Harry Ng on 11/6/14.
+//  Copyright (c) 2014 Request. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+            
+
+@end
+
+@implementation SecondViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
